3eb16735b24718ba1119c242ebd90373743596a9
Jovanonymous <jovanonymous@gmail.com>
2022-11-07 23:15:25 +0700

    upload gslc1

---
+++ resources/views/fastfood/menu.blade.php
@@ -0,0 +1,29 @@
+@include('layouts.nav')
+@extends('reusable.template')
+
+    @for($i=0;$i<count($fastfood);$i++)
+        <div class="d-flex justify-content-around img-rounded">
+            <div class="card" style="width: 20rem;">
+                <div>
+                    <img class="card-img-top rounded mx-auto d-block" style="margin:20px; width:200px" src={{ $photo[$i] }} alt="Card image cap">
+                </div>
+            <div class="card-body">
+                <h5 class="card-title justify-content-center" style="text-align:center">{{ $fastfood[$i] }}</h5>
+                <p class="card-text" style="text-align:center">{{ $description[$i] }}</p>
+                <a href="https://www.google.com" class="btn btn-primary d-flex justify-content-center" style="margin-bottom: 15px; text-align:center; align-items:center">Order now</a>
+                <h5 class="card-text" style="text-align:center"> Stock : {{ $stocks[$i] }}</h5>
+                @if($stocks[$i]==0)
+                    <div class="alert alert-danger d-inline-bock" style="text-align:center">
+                        Not Ready
+                    </div>
+                @else
+                    <div class="alert alert-success d-inline-bock" style="text-align:center">
+                        Ready
+                    </div>
+                @endif
+            </div>
+        </div>
+    @endfor
+</div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/jfc', function () {
+    $arrFastFood = ['Fried Chickin', 'Root Biir'];
+    $photoPath = ['https://i.ibb.co/hVFZQDB/image-removebg-preview-1.png', 'https://i.ibb.co/P9tpbdH/image-removebg-preview-3.png'];
+    $description = ['This fried chickin will smack your tounge like a bee.','This rootbiir kinda makes u feel dizzy and lazy.'];
+    $stock = ['25', '0'];
+    return view('fastfood/menu')
+    ->with('fastfood', $arrFastFood)
+    ->with('photo', $photoPath)
+    ->with('description', $description)
+    ->with('stocks', $stock);
+});
